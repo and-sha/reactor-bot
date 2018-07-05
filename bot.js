598eec8bd344e910a2d0bf59f04d915be7923d5a
and-sha <andrey.shatkovsky@gmail.com>
2018-07-05 12:57:43 +1100

    reactor превращается, reactor превращается... в миниинда :D

--- bot.js
+++ bot.js
@@ -1,36 +1,47 @@
 const Eris = require("eris");
-var bot = new Eris.CommandClient("NDYzMTIyMjA0Mzc3MjE5MDgy.Dhr18Q.gh6FhWTuYDCut7pU_qObEt43BrE", {}, {
-    description: "Оно по-идее будет реагировать",
+var bot = new Eris.CommandClient("NDI2NTUzMjUyNjk2MjkzMzc2.Dh8H5w.XuCFAVKv7nAd4fBjN1JnVjMrWvY", {}, {
+    description: "Бот с разными полезностями для Просто Сервера",
     owner: "andysha#2148",
     prefix: "*"
 });
 bot.on("ready", () => {
     console.log("Готов!");
-    bot.editStatus("online", {name: "мемасики", type: 3});
+    bot.editStatus("online", {name: "мемасики | *help", type: 3});
 });
 
 bot.registerCommandAlias("помощь", "help");
 bot.on("messageCreate", (msg) => {
+  //Функционал для обоих серверов
   if(msg.mentionEveryone){
     msg.addReaction("FeelsPingedMan:462831919848095755");
     console.log(`${msg.author.username} всех призвал!`);
   }
+  if(typeof msg.mentions[0] !== 'undefined'){
+    msg.mentions.forEach(item => {
+      if(item.id == 463122204377219082){
+        console.log(`${msg.author.username} меня призвал!`);
+        msg.channel.createMessage("Не надо меня призывать, *help - помощь по командам");
+        return;
+      }
+    });
+  }
+  //Функционал для ОНЛ
   if(msg.channel.id == 352033994587504640 && ( typeof msg.attachments[0] !== 'undefined' || msg.content.search('((?:http|https)(?::\\/{2}[\\w]+)(?:[\\/|\\.]?)(?:[^\\s"]*))') !== -1)){
-    console.log("Заценил мемасик");
+    console.log("Заценил мемасик в ОНЛ");
     msg.addReaction("thonk:380570863873032192");
     msg.addReaction("lul1:463923338851385345");
     msg.addReaction("lul2:463923398079283201");
     msg.addReaction("lul3:463923428135796737");
     msg.addReaction("lul4:463923464420458516");
   }
-  if(typeof msg.mentions[0] !== 'undefined'){
-    msg.mentions.forEach(item => {
-      if(item.id == 463122204377219082){
-        console.log(`${msg.author.username} меня призвал!`);
-        msg.channel.createMessage("зо што");
-        return;
-      }
-    });
+  //Функционал для Просто Сервера
+  if(msg.channel.id == 371446270264606742 && ( typeof msg.attachments[0] !== 'undefined' || msg.content.search('((?:http|https)(?::\\/{2}[\\w]+)(?:[\\/|\\.]?)(?:[^\\s"]*))') !== -1)){
+    console.log("Заценил мемасик на Просто Сервере");
+    msg.addReaction("thonk:380570863873032192");
+    msg.addReaction("lul1:463923338851385345");
+    msg.addReaction("lul2:463923398079283201");
+    msg.addReaction("lul3:463923428135796737");
+    msg.addReaction("lul4:463923464420458516");
   }
 });
 
