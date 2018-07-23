4a507908badb688f636bfb49c340035d4c3bbfb3
and-sha <andrey.shatkovsky@gmail.com>
2018-07-23 12:33:41 +1100

    Исправление лога для массово удалённых сообщений

--- bot.js
+++ bot.js
@@ -85,6 +85,7 @@ bot.on("messageDelete", (msg) => {
 bot.on("messageDeleteBulk", (msgs) => {
   msgs.forEach(msg => {
     if (typeof msg.content !== 'undefined'){
+      var date = new Date(msg.timestamp);
       if (typeof msg.attachments[0] !== 'undefined'){
         bot.createMessage("470759072967426049", `Удалено сообщение от ${msg.author.username}, написанное ${date.toUTCString()}: "${msg.content}".\nИнформация о прикреплениях: ${msg.attachments}`);
       } else {
