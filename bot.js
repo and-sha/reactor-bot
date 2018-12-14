c916e054de35b01502d5b9375d26f23811ba04d8
and-sha <34363366+and-sha@users.noreply.github.com>
2018-12-14 20:43:03 +1100

    Вроде всё

--- bot.js
+++ bot.js
@@ -72,7 +72,12 @@ bot.on("messageReactionAdd", (msg, emoji, id) =>{
     msg.removeReaction("rolf:469056924625928202", id);
   };
   if(emoji.id == "469056924625928202" && msg.author.id != id){
-    var count = msg.reactions.get("rolf:469056924625928202").count
+    var count = 0;
+    msg.reactions.forEach(emote => {
+      if(emote.emoji.id == "469056924625928202"){
+        count = emote.count
+      };
+    });
     if(count > 2){
       msg.pin()
     };
